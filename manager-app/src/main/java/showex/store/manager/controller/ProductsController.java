c6ad5bbae766a00ed96a5276146b3ff10b78c28b
showex <orehnic@gmail.com>
2024-03-15 11:36:25 +0300

    basic-web-app

---
+++ manager-app/src/main/java/showex/store/manager/controller/ProductsController.java
@@ -0,0 +1,45 @@
+package showex.store.manager.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.BindErrorUtils;
+import showex.store.manager.controller.payload.NewProductPayload;
+import showex.store.manager.entity.Product;
+import showex.store.manager.service.ProductService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("catalogue/products")
+public class ProductsController {
+    private final ProductService productService;
+
+    @GetMapping("list")
+    public String getProductsList(Model model) {
+        model.addAttribute("products", this.productService.findAllProducts());
+        return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getNewProductPage() {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(@Valid NewProductPayload payload, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
+    }
+}
---
+++ manager-app/src/main/java/showex/store/manager/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package showex.store.manager.repository;
+
+import showex.store.manager.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
